6e456a5d240737e28b20b30398a06ca94dc83e06
Amir Muhammad Hakim <amir.muh.hakim@gmail.com>
2021-02-12 10:46:05 +0700

    ✨ Finishing meal detail screen

--- screens/MealDetailScreen.tsx
+++ screens/MealDetailScreen.tsx
@@ -1,5 +1,5 @@
 import React, { FC, useLayoutEffect } from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, ScrollView, Image } from "react-native";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
@@ -8,6 +8,9 @@ import {
   MealDetailScreenRouteProp,
 } from "../navigation/MealsStack/types";
 import AppHeaderButton from "../components/AppHeaderButton";
+import BodyText from "../components/Text/BodyText";
+import Colors from "../constants/colors";
+import Fonts from "../constants/fonts";
 
 const MealDetailScreen: FC = () => {
   const navigation = useNavigation<MealDetailScreenNavProp>();
@@ -25,22 +28,56 @@ const MealDetailScreen: FC = () => {
   }, [navigation, params.meal.title]);
 
   return (
-    <View style={styles.screen}>
-      <Text>The Meal Detail Screen</Text>
-      <Button
-        title="GO BACK TO CATEGORIES"
-        onPress={() => navigation.popToTop()}
-      />
-      <Button title="GO BACK TO MEALS" onPress={() => null} />
-    </View>
+    <ScrollView>
+      <Image style={styles.image} source={{ uri: params.meal.imageUrl }} />
+      <View style={styles.approximation}>
+        <BodyText>{params.meal.duration}m</BodyText>
+        <BodyText>{params.meal.upperComplexity}</BodyText>
+        <BodyText>{params.meal.upperAffordability}</BodyText>
+      </View>
+      <View style={styles.howTosContainer}>
+        <Text style={styles.howToLabel}>Ingredients</Text>
+        {params.meal.ingredients.map((ingredient) => (
+          <View style={styles.howToItem} key={ingredient}>
+            <BodyText>{ingredient}</BodyText>
+          </View>
+        ))}
+        <Text style={styles.howToLabel}>Steps</Text>
+        {params.meal.steps.map((step) => (
+          <View style={styles.howToItem} key={step}>
+            <BodyText>{step}</BodyText>
+          </View>
+        ))}
+      </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+  image: {
+    width: "100%",
+    height: 200,
+  },
+  approximation: {
+    flexDirection: "row",
+    padding: 15,
+    justifyContent: "space-around",
+    backgroundColor: Colors.PrimaryLight,
+  },
+  howTosContainer: {
+    marginTop: 10,
+  },
+  howToLabel: {
+    fontFamily: Fonts.OpenSansBold,
+    fontSize: 22,
+    textAlign: "center",
+  },
+  howToItem: {
+    marginVertical: 10,
+    marginHorizontal: 20,
+    borderColor: "#ccc",
+    borderWidth: 1,
+    padding: 10,
   },
 });
 
